b269a4f5032d49ed1faddd5c0a7809b29a442266
Rory N <rorynickolls@gmail.com>
2021-01-29 20:44:42 +0000

    Comments

--- Assets/Scripts/IslandGenerator.cs
+++ Assets/Scripts/IslandGenerator.cs
@@ -17,19 +17,26 @@ public class IslandGenerator : MonoBehaviour
         }
     }
 
+    // Generates a chain of islands with the specified parameters
     private List<Vector3> GenerateIslandChain(Vector3 startPos, Vector3 islandsDir, float minHopDist, float maxHopDist, float maxHopAngle, float chainChance, int minChainLength, int maxChainLength)
     {
+        // Add start position
         List<Vector3> positions = new List<Vector3>();
         positions.Add(startPos);
 
+        // Randomise chain length and loop to end of it
         int chainLength = Random.Range(minChainLength, maxChainLength);
+
         Vector3 lastIslandPos = startPos;
         for (int j = 0; j < chainLength; j++)
         {
+            // Choose direction for next island slightly offset from main islandsDir
+            // Then calculate next island position
             Vector2 nextDir = Quaternion.Euler(0, 0, Random.Range(-10, 10)) * islandsDir;
             lastIslandPos = NextIslandPosition(lastIslandPos, nextDir, Random.Range(minHopDist, maxHopDist), Random.Range(-maxHopAngle / 2, maxHopAngle / 2));
             positions.Add(lastIslandPos);
 
+            // If chainChance is met then recursively start a new chain at a 60-120 degree angle with lower chances of more subchains
             float chance = Random.Range(0f, 1f);
             if (chance < chainChance)
             {
@@ -42,6 +49,7 @@ public class IslandGenerator : MonoBehaviour
         return positions;
     }
 
+    // Generates the next island position given some parameters
     private Vector3 NextIslandPosition(Vector3 lastPos, Vector3 dir, float hopDist, float hopAngle)
     {
         return lastPos + (Quaternion.Euler(0, 0, hopAngle) * dir * hopDist);

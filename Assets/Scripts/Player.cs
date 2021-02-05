f76194212f67bf99c8f9305b568212ffdd8cfe7f
Hazzardy <harry.dacombe@ntlworld.com>
2021-02-05 18:06:48 +0000

    :wq

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,28 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Player : MonoBehaviour
 {
 
-    [SerializeField]
-    private float movementSpeed = 5.0f;
+    // Reference to the actual ship object
+    [SerializeField] private GameObject PlayerGameObject;
 
-    [SerializeField]
-    private float steeringSpeed = 1.0f;
+    // Ship variables stored locally so functions don't need to be repeatedly called
+    private float hHealth;
+    private float mCannons;
+    private float cDamage;
+    private float fRate;
+    private float tSpeed;
+    private float mSpeed;
+    private float currentRotation;
 
-    private float rotation = 0f;
-
+    // A reference to the sprite renderer
     private SpriteRenderer spriteRenderer;
+
+    // References to the UI elements and player resources
+    private float playerGold;
+    private float playerHullPoints;
+    private float playerLoadedCannons;
+    [SerializeField] private GameObject GoldReference;
+    [SerializeField] private GameObject HullReference;
+    [SerializeField] private GameObject CannonsReference;
+
+    // References to the UI objects for ease of toggling
+    [SerializeField] private GameObject ShopObjectReference;
+    [SerializeField] private GameObject PauseMenuReference;
 
     private void Start()
     {
-        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        InitialiseGameWorld();
     }
 
     // Update is called once per frame
     void Update()
     {
+        UpdateShipDesiredLocation();
+    }
+
+    private void UpdateShipDesiredLocation()
+    {
+
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
@@ -33,10 +57,10 @@ public class Player : MonoBehaviour
             Camera.main.transform.localPosition = newCameraPos;
         }
 
-        rotation -= horizontal * steeringSpeed * Time.deltaTime;
-        spriteRenderer.transform.rotation = Quaternion.Euler(0, 0, rotation);
+        currentRotation -= horizontal * tSpeed * Time.deltaTime;
+        spriteRenderer.transform.rotation = Quaternion.Euler(0, 0, currentRotation);
 
-        Vector3 velocity = spriteRenderer.transform.up * vertical * movementSpeed;
+        Vector3 velocity = spriteRenderer.transform.up * vertical * mSpeed;
 
         float newX = transform.position.x + velocity.x * Time.deltaTime;
         float newY = transform.position.y + velocity.y * Time.deltaTime;
@@ -52,5 +76,73 @@ public class Player : MonoBehaviour
         }
 
         transform.position += velocity * Time.deltaTime;
+
     }
+
+    // I'm not sure what the performance impact is of this, figured not GetComponent every frame was an
+    // improvement but maybe it undermines the point of making a derived class for ship.
+    private void UpdateShipStats()
+    {    
+        hHealth = PlayerGameObject.GetComponent<PlayerShip>().HullHealth;    
+        mCannons = PlayerGameObject.GetComponent<PlayerShip>().MaxCannonballs;   
+        cDamage = PlayerGameObject.GetComponent<PlayerShip>().CannonballDamage;   
+        fRate = PlayerGameObject.GetComponent<PlayerShip>().FireRate;
+        mSpeed = PlayerGameObject.GetComponent<PlayerShip>().MovementSpeed;
+        tSpeed = PlayerGameObject.GetComponent<PlayerShip>().TurningSpeed;
+    }
+
+    private void UpdatePlayerUI()
+    {
+        GoldReference.GetComponent<TMP_Text>().text = playerGold.ToString();
+        HullReference.GetComponent<TMP_Text>().text = playerHullPoints.ToString();
+        CannonsReference.GetComponent<TMP_Text>().text = playerLoadedCannons.ToString();
+    }
+
+    private void ToggleActive(GameObject Object)
+    {
+        if(Object.activeInHierarchy == true)
+        {
+            Object.SetActive(false);
+        }
+        else
+        {
+            Object.SetActive(true);
+        }
+    }
+
+    private void InitialiseGameWorld()
+    {
+        // Starting variables and update player UI to match
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        UpdateShipStats();
+        playerGold = 100f;
+        playerHullPoints = hHealth;
+        playerLoadedCannons = mCannons;
+        UpdatePlayerUI();
+        ShopObjectReference.SetActive(false);
+        PauseMenuReference.SetActive(false);
+    }
+
+    private void OnGUI()
+    {
+        Event e = Event.current;
+        if (e.isKey)
+        {
+            if (e.keyCode == KeyCode.Escape)
+            {
+                if(PauseMenuReference.activeInHierarchy == true)
+                {
+                    PauseMenuReference.SetActive(false);
+                    return;
+                }
+                if(PauseMenuReference.activeInHierarchy == false)
+                {
+                    PauseMenuReference.SetActive(true);
+                    return;
+                }
+            }
+        }
+    }
+
+
 }
--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -4,11 +4,47 @@ using UnityEngine;
 
 public abstract class Ship : MonoBehaviour
 {
-    [SerializeField] private float hullHealth;
-    [SerializeField] private float fireRate;
 
-    [SerializeField] private float movementSpeed;
-    [SerializeField] private float turningSpeed;
+    // Added getters and setters and default values. Might be wrong but explain later if this is a mistake
+
+    [SerializeField] private float hullHealth = 100f;
+    public float HullHealth
+    {
+        get { return hullHealth; }
+        set { hullHealth = value; }
+    }
+    [SerializeField] private float fireRate = 10f;
+    public float FireRate
+    {
+        get { return fireRate; }
+        set { fireRate = value; }
+    }
+    [SerializeField] private float maxCannonballs = 10f;
+    public float MaxCannonballs
+    {
+        get { return maxCannonballs; }
+        set { maxCannonballs = value; }
+    }    
+    [SerializeField] private float cannonballDamage = 10f;
+    public float CannonballDamage
+    {
+        get { return cannonballDamage; }
+        set { cannonballDamage = value; }
+    }
+    [SerializeField] private float movementSpeed = 1f;
+    public float MovementSpeed
+    {
+        get { return movementSpeed; }
+        set { movementSpeed = value; }
+    }
+    [SerializeField] private float turningSpeed = 30f;
+    public float TurningSpeed
+    {
+        get { return turningSpeed; }
+        set { turningSpeed = value; }
+    }
+
+
 
     protected Vector2 velocity;
 
